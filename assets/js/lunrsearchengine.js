81ca5c81b26b852cdd0ade68515108fa67d88267
nhamngoctuananh <vucute9366@yahoo.com.vn>
2022-07-30 11:30:18 +0700

    ư

--- assets/js/lunrsearchengine.js
+++ assets/js/lunrsearchengine.js
@@ -29,6 +29,12 @@ var idx = lunr(function () {
     this.field('title')
     this.field('body')
     this.field('description')
+    documents = documents.map(doc=>({
+        description: removeAccents(doc.description)||"",
+        title: removeAccents(doc.title)||"",
+        body: removeAccents(doc.body)||"",
+        id:doc.id||0,
+        url:doc.url||""}))
     documents.forEach(function (doc) {
         this.add(doc)
     }, this)
@@ -37,11 +43,11 @@ function lunr_search(term) {
     document.getElementById('lunrsearchresults').innerHTML = '<ul></ul>';
     if(term) {
         document.getElementById('lunrsearchresults').innerHTML = "<p>Tìm kiếm với từ khóa: '" + term + "'</p>" + document.getElementById('lunrsearchresults').innerHTML;
-        var termRemoveAccentEd = (removeAccents(term)).toString().toLowerCase();
+        var termRemoveAccentEd = removeAccents(term);
         //put results on the screen.
         var results = idx.search(termRemoveAccentEd);
         if(results.length>0){
-            console.log(idx.search(term));
+            //console.log(idx.search(term));
             //if results
             for (var i = 0; i < results.length; i++) {
                 // more statements
@@ -64,13 +70,12 @@ function lunr_search(term) {
 
     document.getElementById('lunrsearchresults').innerHTML = '<div id="resultsmodal" class="modal fade show d-block"  tabindex="-1" role="dialog" aria-labelledby="resultsmodal"> <div class="modal-dialog shadow" role="document"> <div class="modal-content"> <div class="modal-header" id="modtit"> <button type="button" class="close" id="btnx" data-dismiss="modal" aria-label="Close"> &times; </button> </div> <div class="modal-body"> <ul class="mb-0"> </ul>    </div> <div class="modal-footer"><button id="btnx" type="button" class="btn btn-primary btn-sm" data-dismiss="modal">Close</button></div></div> </div></div>';
     if(term) {
-        var termRemoveAccentEd = (removeAccents(term)).toString().toLowerCase();
+        var termRemoveAccentEd = removeAccents(term);
         document.getElementById('modtit').innerHTML = "<h5 class='modal-title'>Search results for '" + term + "'</h5>" + document.getElementById('modtit').innerHTML;
         //put results on the screen.
         var results = idx.search(termRemoveAccentEd);
         if(results.length>0){
-            console.log("search 2");
-            console.log(idx.search(term));
+            //console.log(idx.search(term));
             //if results
             for (var i = 0; i < results.length; i++) {
                 // more statements
@@ -115,5 +120,5 @@ function removeAccents(str) {
       var char = AccentsMap[i][0];
       str = str.replace(re, char);
     }
-    return str;
+    return (str).toString().toLowerCase();
   }
