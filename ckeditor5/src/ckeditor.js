0ae138acbc58a52b02b781260107c1475c5ae060
nhamngoctuananh <vucute9366@yahoo.com.vn>
2022-10-07 05:42:57 +0700

    m

--- ckeditor5/src/ckeditor.js
+++ ckeditor5/src/ckeditor.js
@@ -4,12 +4,12 @@
  */
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
-import AutoImage from '@ckeditor/ckeditor5-image/src/autoimage.js';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
 import CKBox from '@ckeditor/ckeditor5-ckbox/src/ckbox.js';
 import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices.js';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
+import GeneralHtmlSupport from '@ckeditor/ckeditor5-html-support/src/generalhtmlsupport.js';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
 import Image from '@ckeditor/ckeditor5-image/src/image.js';
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption.js';
@@ -21,7 +21,6 @@ import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
 import Link from '@ckeditor/ckeditor5-link/src/link.js';
 import List from '@ckeditor/ckeditor5-list/src/list.js';
 import Markdown from '@ckeditor/ckeditor5-markdown-gfm/src/markdown.js';
-import MathType from '@wiris/mathtype-ckeditor5';
 import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed.js';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice.js';
@@ -35,12 +34,12 @@ class Editor extends ClassicEditor {}
 // Plugins to include in the build.
 Editor.builtinPlugins = [
 	Autoformat,
-	AutoImage,
 	BlockQuote,
 	Bold,
 	CKBox,
 	CloudServices,
 	Essentials,
+	GeneralHtmlSupport,
 	Heading,
 	Image,
 	ImageCaption,
@@ -52,7 +51,6 @@ Editor.builtinPlugins = [
 	Link,
 	List,
 	Markdown,
-	MathType,
 	MediaEmbed,
 	Paragraph,
 	PasteFromOffice,
