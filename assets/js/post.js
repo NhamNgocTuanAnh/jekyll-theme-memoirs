aaf4a526ed9fdbe5d09eef73165ca6ee9d5d0ff2
nhamngoctuananh <vucute9366@yahoo.com.se>
2025-01-03 13:43:23 +0700

    a

--- assets/js/post.js
+++ assets/js/post.js
@@ -2,24 +2,5 @@ const colors = ["#FF0000", "#006400", "#FFFFFF", "#FFD700", "#D2691E"], snowflak
 let isScriptEnabled=!0;const triggerElement=document.getElementById("triggerLoadElfSightScript");const getRandomElement=(arr)=>arr[Math.floor(Math.random()*arr.length)];const isElementInViewport=(el)=>{if(!el)return!0;const rect=el.getBoundingClientRect();return rect.top<window.innerHeight&&rect.bottom>=0};const insertDiv=(targetId,divId)=>{const paragraphs=document.querySelectorAll("#articlePost section p");if(paragraphs.length===0){console.error("Không tìm thấy thẻ <p> nào trong section");return}
   const randomParagraph=paragraphs[Math.floor(Math.random()*paragraphs.length)];const newDiv=document.createElement("div");newDiv.id=divId;const instaShowDiv=document.createElement("div");instaShowDiv.id="instaShow2";randomParagraph.after(newDiv,instaShowDiv)};insertDiv("main-content-content","triggerLoadElfSightScript");const instaShowDiv=document.getElementById("instaShow2");if(instaShowDiv){if(isElementInViewport(triggerElement)&&isScriptEnabled){const authorMeta=document.querySelector('meta[name="author"][content="minh"]');const locationUrl=window.location.href;if(!locationUrl.includes("localhost")&&!locationUrl.includes("127.0.0.1")){const appIds=authorMeta?["elfsight-app-4bab93fa-028b-4ab6-a095-3bdcecb91616","elfsight-app-fae669a8-2109-47be-8003-7767f2c82a5a"]:["elfsight-app-58eefcb9-7b01-4eda-a0ed-b18b31f127fc","elfsight-app-5251747b-b6d4-4dd8-83b2-d4c39ed9caea"];const randomAppId=getRandomElement(appIds);const newDiv=document.createElement("div");newDiv.className=randomAppId;newDiv.setAttribute("data-elfsight-app-lazy","");newDiv.style.textAlign="center";instaShowDiv.appendChild(newDiv)}else{console.log("Đang chạy localhost, không thêm lệnh script.")}}}else{console.error('Không tìm thấy thẻ div có id "instaShow2".')}(()=>{const script=document.createElement("script");script.src="https://static.elfsight.com/platform/platform.js";script.setAttribute("data-use-service-core","");script.defer=!0;document.head.appendChild(script);console.log("Tải loadElfSightScript https://static.elfsight.com/platform/platform.js")})();const bottomOverlay=document.getElementById("bottom-overlay");const closeBtn=document.getElementById("read-more-btn-hell");closeBtn.addEventListener("click",()=>{bottomOverlay.style.display="none"});  function handleClickReadMoreBtn(){const readMoreBtn=document.getElementById("read-more-btn-hell");if(readMoreBtn){readMoreBtn.style.display="none"}
   for(;;){const overlays=document.querySelectorAll(".content-overlay");if(overlays){if(overlays.length>0){for(const overlay of overlays){overlay.style.maxHeight="none"}};window.scrollTo({top:456,behavior:"smooth"});break}}
-  if (typeof ga === "function") {
-    ga("send", "event", "read-new", "read-more", "{{ page.url }}", 10);
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-    const readMoreBtn = document.getElementById("read-more-btn-hell");
-    if (readMoreBtn) {
-        readMoreBtn.addEventListener("click", () => {
-            readMoreBtn.style.display = "none";
-            const overlays = document.querySelectorAll(".content-overlay");
-            overlays.forEach(overlay => {
-                overlay.style.maxHeight = "none";
-            });
-            window.scrollTo({ top: 456, behavior: "smooth" });
-
-            if (typeof ga === "function") {
-                ga("send", "event", "read-new", "read-more", "{{ page.url }}", 10);
-            }
-        });
-    }
-});
+  if(typeof ga==="function"){ga("send","event","read-new","read-more","{{ page.url }}",10)}}
+  document.addEventListener("DOMContentLoaded",()=>{const readMoreBtn=document.getElementById("read-more-btn-hell");if(readMoreBtn){readMoreBtn.addEventListener("click",handleClickReadMoreBtn)}})
