75e5a0d52abed645d2fe341881ec8306c37f810a
nhamngoctuananh <vucute9366@yahoo.com.se>
2025-01-03 23:10:46 +0700

    a

--- assets/js/tet.js
+++ assets/js/tet.js
@@ -71,3 +71,55 @@ if (ie4up || ns6up) {
     snowIE_NS6();
     if (hidesnowtime > 0) setTimeout('hidesnow()', hidesnowtime * 1000)
 }
+window.addEventListener("load", function() {
+    function getValidatedUrl(url) {
+        const index = url.indexOf("https://");
+        return index !== -1 ? url.slice(index) : url;
+    }
+
+    const imageUrl = getValidatedUrl("{{ site.baseurl }}/{{ page.image }}");
+
+    const entryHeader = document.querySelector('.entry-header');
+    if (entryHeader) {
+        entryHeader.style.display = "block";
+    }
+
+    const featuredImageContainer = document.getElementById("idFeaturedImage");
+    if (featuredImageContainer) {
+        const originalImage = document.querySelector("img.original-image-selector"); // Adjust the selector as needed
+        const img = document.createElement("img");
+        img.setAttribute("class", "featured-image lazyimg");
+        img.setAttribute("src", imageUrl);
+
+        if (originalImage && originalImage.hasAttribute("alt")) {
+            img.setAttribute("alt", originalImage.getAttribute("alt"));
+        }
+
+        img.onload = function() {
+            if (img.hasAttribute("loaded")) {
+                img.removeAttribute("loaded");
+            }
+        };
+
+        featuredImageContainer.appendChild(img);
+    }
+
+    // Add Tet elements to the head
+    const head = document.head;
+
+    const tetLeft = document.createElement("div");
+    tetLeft.setAttribute("class", "tet_left");
+    tetLeft.innerHTML = '<img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgb3PBtYvcb0AwzJvp5rDfHAOlC_0BsSevp3we6JTICzZAbrhaLcMtlnOAvCb_6A_LuZfnDSCYUVp5M6a_SSq1tQ825ioZ3H1mkboSV8OA7y-cqo723yZJVuniaCibs715peZlIoVfENoBVvigVfJJDffiAoij6tWFv4Jx3RWIVUcQDN5zeMWweAuCuCw/s16000/left-1.png" alt="AnonyViet-Tet"/>';
+
+    const tetRight = document.createElement("div");
+    tetRight.setAttribute("class", "tet_right");
+    tetRight.innerHTML = '<img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhmE7w44KuUqFCci75HHT6fo4Snv-s9j8NpP9ERbJhtT9WOasSnDeiDGruspoNUbHQBENce0laVEZZaQc0C-V5pZBPhNgX2ktJprmXpW96EgMHVBBDdbO2UugIYyNY-nXST_nKFcDwDXDiBqM7LZktNIrkgDqQEtdnvYxtt5ZCoV82VNKgSYUIxT1OzYg/s16000/right-1.png" alt="AnonyViet-Tet"/>';
+
+    const tetBottom = document.createElement("div");
+    tetBottom.setAttribute("class", "tet_bottom");
+    tetBottom.innerHTML = '<img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgwruFlhClo3FUKNBQtDvqWYiDVOoi-IT7Jy4R11OU5HaOFR2N7CcX5sH4FWQI_GRoVrx4Hd5pVQREJ_QsAjvSA41v25TW0LEGW2jb8s3J2QwCrXp4qsMqdvxUZz9lglGyxL4YQxIbbf17zyqd99Rr28rDzx-foaXJRQ13kQUAblMtlt4U1rKMYbHkn5w/s16000/bottom-1.png" alt="AnonyViet-Tet"/>';
+
+    head.appendChild(tetLeft);
+    head.appendChild(tetRight);
+    head.appendChild(tetBottom);
+});
