586fef65cb795badee9798f78066d0b779e60c0d
nhamngoctuananh <vucute9366@yahoo.com.se>
2025-01-04 08:49:56 +0700

    s

--- assets/js/tet.js
+++ assets/js/tet.js
@@ -1,69 +1,95 @@
-
-// Gọi hàm để tạo hiệu ứng
+// Lấy canvas và ngữ cảnh vẽ
 const canvas = document.getElementById("fireworksCanvas");
 const ctx = canvas.getContext("2d");
 
+// Khởi tạo kích thước canvas
 function initCanvas() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 }
-
 initCanvas();
 window.addEventListener("resize", initCanvas);
 
+// Lớp hạt pháo hoa
 class Particle {
     constructor(x, y, color) {
+        this.reset(x, y, color);
+    }
+
+    reset(x, y, color) {
         this.x = x;
         this.y = y;
-        this.color = color;
+        this.color = color || colors[Math.floor(Math.random() * colors.length)];
         this.size = Math.random() * 5 + 1;
         this.speedX = Math.random() * 4 - 2;
         this.speedY = Math.random() * 4 - 2;
         this.life = Math.random() * 30 + 50;
+        this.active = true;
     }
 
     update() {
-        this.x += this.speedX;
-        this.y += this.speedY;
-        this.size *= 0.96;
-        this.life--;
+        if (this.life > 0 && this.size > 0.1) {
+            this.x += this.speedX;
+            this.y += this.speedY;
+            this.size *= 0.96;
+            this.life--;
+        } else {
+            this.active = false;
+        }
     }
 
     draw() {
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-        ctx.fillStyle = this.color;
-        ctx.fill();
+        if (this.active) {
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+            ctx.fillStyle = this.color;
+            ctx.fill();
+        }
     }
 }
 
-const particles = [];
+// Biến và danh sách pháo hoa
+const particlesPool = [];
+const activeParticles = [];
 const colors = ["#ff5733", "#33ff57", "#5733ff", "#f4ff33", "#ff33e3"];
 
+// Hàm tạo pháo hoa
 function createFireworks(x, y) {
     for (let i = 0; i < 50; i++) {
-        const color = colors[Math.floor(Math.random() * colors.length)];
-        particles.push(new Particle(x, y, color));
+        let particle = particlesPool.pop();
+        if (!particle) {
+            particle = new Particle(x, y);
+        } else {
+            particle.reset(x, y);
+        }
+        activeParticles.push(particle);
     }
 }
 
+// Hàm hoạt hình
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    particles.forEach((particle, index) => {
-        if (particle.life <= 0 || particle.size <= 0.1) {
-            particles.splice(index, 1);
-        } else {
-            particle.update();
-            particle.draw();
-        }
+    activeParticles.forEach((particle) => {
+        particle.update();
+        particle.draw();
     });
+
+    // Lọc và đưa các hạt không còn hoạt động về danh sách tái sử dụng
+    for (let i = activeParticles.length - 1; i >= 0; i--) {
+        if (!activeParticles[i].active) {
+            particlesPool.push(activeParticles[i]);
+            activeParticles.splice(i, 1);
+        }
+    }
 
     requestAnimationFrame(animate);
 }
 
+// Gắn sự kiện tạo pháo hoa khi di chuột
 document.addEventListener("mousemove", (e) => {
     createFireworks(e.clientX, e.clientY);
 });
 
+// Bắt đầu hoạt hình
 animate();
