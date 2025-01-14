07f3ade7ef1243407346a1e8c0dd5de861f69318
nhamngoctuananh <vucute9366@yahoo.com.se>
2025-01-14 17:48:37 +0700

    a

--- assets/js/main.js
+++ assets/js/main.js
@@ -188,10 +188,4 @@ function isTetDate() {
       firstScript.parentNode.insertBefore(script, firstScript);
     })(document, "script", "https://chimpstatic.com/mcjs-connected/js/users/a439f78ba77fd2f9e892d4fa8/f37836a9f614fff78ac17eacf.js");
   });
-  const midAutumnMessage=document.getElementById("mid-autumn-message");if(midAutumnMessage)midAutumnMessage.style.display="block";break;case 1:case 2:addLink('/assets/css/tet.css');createSnowfallEffect();break}})
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-T53P5GTK');
 
